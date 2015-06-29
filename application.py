41d1403e7f928e7a082c71475736c7bcba92d9c0
Colt <colt.daily@gmail.com>
2015-06-29 11:39:11 +0000

    Removed mpd and replaced with vlc library

--- application.py
+++ application.py
@@ -2,30 +2,17 @@ __author__ = 'colt'
 
 from screen_main import MainScreen
 from tkinter import *
-from mpd import MPDClient
 
 class Application:
     def __init__(self, master):
         self.master = master
-        self.connect_mpd()
         self.create_screens()
         self.apply_fullscreen()
-
-    def connect_mpd(self):
-        self.client = MPDClient()
-        self.client.timeout = 10
-        self.client.idletimeout = None
-    """ self.client.connect("localhost", 6600)
-        print(self.client.mpd_version)
-        print(self.client.find("any", "house"))
-        self.client.close()
-        self.client.disconnect() """
 
     def create_screens(self):
         MainScreen(self.master)
 
     def apply_fullscreen(self):
-      #  self.master.attributes('-zoomed', True)
         self.fullscreen_state = False
         self.master.bind("<F11>", self.toggle_fullscreen)
         self.master.bind("<Escape>", self.end_fullscreen)
